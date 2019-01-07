ee2b5866d6c549a35a5609273d5b33cf6adbb9d2
Daniel Brewer <danielbrewer2014@comcast.net>
2019-01-07 14:57:36 -0600

    Listener added and working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,6 @@ import contactList from "./ContactList"
 
 contactForm.createHTML()
 contactList.contactify()
+
+// let saveButton = document.querySelector(".contact_save");
+// saveButton.addEventListener("click", contactForm.buttonClick)
